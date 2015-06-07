97209c2ea30ee1540b5e399ab87d41cb4a0e23d4
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-06-07 16:02:49 +0200

    Devise working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :categories
   resources :words
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'pages#home'
+  root 'words#index'
 
   post 'create_word_and_category' => 'new_word#create_word_and_category'
 
