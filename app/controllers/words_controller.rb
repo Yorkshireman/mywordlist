ea6ab3c9f7a31a3b05f89fee878597f2d070343a
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-06-08 02:10:52 +0200

    minor change

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -69,6 +69,6 @@ class WordsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def word_params
-      params.require(:word).permit(:title, :description, :user_id)
+      params.require(:word).permit(:title, :description)
     end
 end
