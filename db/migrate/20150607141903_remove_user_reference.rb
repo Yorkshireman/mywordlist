b5eefcb4bc44f79be066ca78764d266ed6b51a27
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-06-08 02:01:26 +0200

    corrected migrations

--- db/migrate/20150607141903_remove_user_reference.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserReference < ActiveRecord::Migration
-  def change
-  	remove_reference(:words, :user, index: true, foreign_key: true)
-  end
-end
--- db/migrate/20150607235713_add_words_reference.rb
+++
@@ -1,5 +0,0 @@
-class AddWordsReference < ActiveRecord::Migration
-  def change
-  	references :user, index: true, foreign_key: true
-  end
-end
