1b9667c464828e181746560ea89f7c42b396fc2d
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-06-10 01:24:57 +0200

    styled alert messages and they fade automatically

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,9 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove(); 
+    });
+}, 5000);
